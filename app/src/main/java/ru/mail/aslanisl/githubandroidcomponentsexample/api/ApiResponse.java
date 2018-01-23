9e4c5258823aee41a0a4a4131cfe4802a7fffe26
Ivan <aslanisl@mail.ru>
2018-01-23 14:17:35 +0300

    added endless user list support

--- app/src/main/java/ru/mail/aslanisl/githubandroidcomponentsexample/api/ApiResponse.java
+++ app/src/main/java/ru/mail/aslanisl/githubandroidcomponentsexample/api/ApiResponse.java
@@ -4,15 +4,9 @@ package ru.mail.aslanisl.githubandroidcomponentsexample.api;
  * Created by Ivan on 19.01.2018.
  */
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.util.ArrayMap;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import retrofit2.Response;
 
@@ -22,23 +16,17 @@ import retrofit2.Response;
  * @param <T>
  */
 public class ApiResponse<T> {
-    private static final Pattern LINK_PATTERN = Pattern
-            .compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"");
-    private static final Pattern PAGE_PATTERN = Pattern.compile("\\bpage=(\\d+)");
-    private static final String NEXT_LINK = "next";
+
     public final int code;
     @Nullable
     public final T body;
     @Nullable
     public final String errorMessage;
-    @NonNull
-    public final Map<String, String> links;
 
     public ApiResponse(Throwable error) {
         code = 500;
         body = null;
         errorMessage = error.getMessage();
-        links = Collections.emptyMap();
     }
 
     public ApiResponse(Response<T> response) {
@@ -61,40 +49,9 @@ public class ApiResponse<T> {
             errorMessage = message;
             body = null;
         }
-        String linkHeader = response.headers().get("link");
-        if (linkHeader == null) {
-            links = Collections.emptyMap();
-        } else {
-            links = new ArrayMap<>();
-            Matcher matcher = LINK_PATTERN.matcher(linkHeader);
-
-            while (matcher.find()) {
-                int count = matcher.groupCount();
-                if (count == 2) {
-                    links.put(matcher.group(2), matcher.group(1));
-                }
-            }
-        }
     }
 
     public boolean isSuccessful() {
         return code >= 200 && code < 300;
-    }
-
-    public Integer getNextPage() {
-        String next = links.get(NEXT_LINK);
-        if (next == null) {
-            return null;
-        }
-        Matcher matcher = PAGE_PATTERN.matcher(next);
-        if (!matcher.find() || matcher.groupCount() != 1) {
-            return null;
-        }
-        try {
-            return Integer.parseInt(matcher.group(1));
-        } catch (NumberFormatException ex) {
-            //TODO catch exception
-            return null;
-        }
     }
 }
